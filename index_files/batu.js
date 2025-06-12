671526e8ea22af509afc097239fb96ee1e5e9e05
Mr-Empty <eempty666@gmail.com>
2025-06-13 02:25:05 +0700

    Delete index_files directory

--- index_files/batu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const images = document.querySelectorAll('.gallery img');
-    images.forEach((img, index) => {
-        img.addEventListener('click', () => {
-            alert(`Ini adalah foto ke-${index + 1}.`);
-        });
-    });
-});
